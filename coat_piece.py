470a46b684318a40c797c769f77bd180e7ef8da6
Bhanu <bhanu@Bhanus-MacBook-Air.local>
2019-12-17 20:03:24 +0530

    Coat Piece

---
+++ coat_piece.py
@@ -0,0 +1,88 @@
+#import numpy as np
+import random
+
+SUITS = ['H', 'S', 'D', 'C']
+
+
+class Card:
+    def __init__(self, num, suit):
+        self.number = num
+        self.suit = suit
+
+
+class Game:
+    def __init__(self):
+        self.winner = None
+
+
+class Player:
+    def __init__(self, num):
+        self.number = num
+        self.card_list = []
+
+    def define_trump(self):
+        max_suit = None
+        suit_count = {"H": 0, "S": 0, "D": 0, "C": 0}
+        for card in self.card_list[0:5]:
+            suit_count[card.suit] += 1
+        max_num = 0
+        for suit in suit_count:
+            if suit_count[suit] >= max_num:
+                max_suit = suit
+        return max_suit
+
+    def playcard(self):
+        raise NotImplementedError
+
+
+class Board:
+    def __init__(self):
+        self.trump = None
+        self.discard_pile = []
+        self.active_cards = []
+        self.deck = []
+
+    def shuffle(self):
+        random.shuffle(self.deck)
+
+    def generate_cards(self):
+        deck = []
+        for suit in SUITS:
+            for rank in range(2,15,1):
+                card = Card(rank, suit)
+                deck.append(card)
+        return deck
+
+    def reset(self):
+        self.trump = None
+        self.discard_pile = []
+        self.active_cards = []
+        self.deck = self.generate_cards()
+
+    def distribute(self, players):
+        players[0].card_list = self.deck[0:5] + self.deck[20:24] + self.deck[36:40]
+        players[1].card_list = self.deck[5:10] + self.deck[24:28] + self.deck[40:44]
+        players[2].card_list = self.deck[10:15] + self.deck[28:32] + self.deck[44:48]
+        players[3].card_list = self.deck[15:20] + self.deck[32:36] + self.deck[48:52]
+        self.deck = []
+
+
+def game_init():
+    game = Game()
+    players = []
+    for i in range(1, 5, 1):
+        player = Player(i)
+        players.append(player)
+
+    print(players[0].number)
+    board = Board()
+    board.reset()
+    board.shuffle()
+    board.distribute(players)
+
+    for card in players[3].card_list:
+        print("card: {},{}".format(card.number,card.suit))
+
+
+if __name__ == '__main__':
+    game_init()
